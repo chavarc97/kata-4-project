3e892dcca50398f3170f4b1399a0d3221b3a8346
Salvador Rodriguez <chava.crb@gmail.com>
2023-08-30 14:02:26 -0600

    (feat modal display): Modal implementation to display pokemon abilities

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 //Selectors
 const searchBar = document.getElementById('search')
 const pokedex = document.getElementById('pokemonRow')
+const modal = document.getElementById('exampleModal')
+const modalBody = document.getElementById('modalBody')
+
 
 
 /**
  * The fetchPokemon function fetches data for the first 150 Pokemon from the PokeAPI and then displays
  * the Pokemon's name, ID, image, and type.
  */
-const fetchPokemon = () => {
+const fetchPokemon = async() => {
     const promises = [];
     for(let i = 1; i <= 150; i ++){
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`
@@ -18,11 +21,20 @@ const fetchPokemon = () => {
         const pokemon = results.map( (data) => ({
             name: data.name,
             id: data.id,
-            image: data.sprites['front_default'],
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${data.id}.png`,
             type: data.types.map( type => 
-                type.type.name).join(', ')
+                type.type.name).join(', '),
+            abilities: data.abilities.map(ability =>
+                ability.ability.name).join(', '),
+            weight: data.weight,
+            height: data.height,
+            baseExperience: data.base_experience
+
+
         }));
+        console.log(pokemon);
         displayPokemon(pokemon);
+        modalPopup(pokemon)
     });
 };
 
@@ -30,11 +42,10 @@ const fetchPokemon = () => {
 /**
  * The function `displayPokemon` takes an array of Pokemon objects, and generates HTML code to display
  * each Pokemon's image, name, type, and a button for more details.
- * @param pokemon - The `pokemon` parameter is an array of objects. Each object represents a Pokemon
+ * The `pokemon` parameter is an array of objects. Each object represents a Pokemon
  * and has the following properties:
  */
 const displayPokemon = (pokemon) => {
-    console.log(pokemon);
     const pokemonRender = pokemon
         .map( (pokeman) =>
             `
@@ -45,12 +56,38 @@ const displayPokemon = (pokemon) => {
                 <p class="card-text">
                 <strong>Type:</strong> ${pokeman.type}
                 </p>
-                <button class="btn btn-danger" id="modalBtn">See details</button>
+                <button type="button" class="btn btn-danger" id="modalBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                See Details
+                </button>
             </div>
           </div>
             `
         ).join('')
+        
         pokedex.innerHTML = pokemonRender
+}
+
+const modalPopup = (pokemon) => {
+    const modalRender = pokemon.map((poke) => 
+        `
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                    
+                    <h5 class="modal-title" id="${poke.name}"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="modalBody">
+            </div>
+            <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+      </div>
+        `
+    ).join('')
+    console.log(pokemon.name);
+    modal.innerHTML = modalRender
 }
 
 fetchPokemon()
