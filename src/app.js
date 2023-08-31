12e0ec1c89f2ee7132588cf5093412acab79a406
Salvador Rodriguez <chava.crb@gmail.com>
2023-08-31 17:25:24 -0600

    (feat): pokemon modal display

--- src/app.js
+++ src/app.js
@@ -1,42 +1,51 @@
 //Selectors
 const searchBar = document.getElementById('search')
+const searchBtn = document.getElementById('searchBtn')
 const pokedex = document.getElementById('pokemonRow')
 const modal = document.getElementById('exampleModal')
 const modalBody = document.getElementById('modalBody')
+let pokemon;
 
 
-
-/**
- * The fetchPokemon function fetches data for the first 150 Pokemon from the PokeAPI and then displays
- * the Pokemon's name, ID, image, and type.
- */
 const fetchPokemon = async() => {
-    const promises = [];
-    for(let i = 1; i <= 150; i ++){
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-    promises.push( fetch(url).then((res) => res.json()))
-    }
-
-    Promise.all(promises).then( results => {
-        const pokemon = results.map( (data) => ({
-            name: data.name,
-            id: data.id,
-            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${data.id}.png`,
-            type: data.types.map( type => 
-                type.type.name).join(', '),
-            abilities: data.abilities.map(ability =>
-                ability.ability.name).join(', '),
-            weight: data.weight,
-            height: data.height,
-            baseExperience: data.base_experience
-
-
-        }));
-        console.log(pokemon);
-        displayPokemon(pokemon);
-        modalPopup(pokemon)
-    });
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+    const res = await fetch(url);
+    const data = await res.json();
+    pokemon = data.results.map( (result, i) => ({
+        name: result.name.toUpperCase(),
+        id: i +1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${i + 1}.png`
+    }))
+    displayPokemon(pokemon)
+       /*  modalPopup(pokemon) */
+    
 };
+
+searchBtn.addEventListener('click', () =>{
+    let pokeSearch = searchBar.value
+    let searchArray = pokemon.filter(((pokeman) => {
+       return pokeman.name == pokeSearch
+    })) 
+    const pokemonRender = searchArray
+        .map( (pokeman) =>
+            `
+            <div class="card col-lg-6 mb-4 ml-5" style="width: 18rem;">
+            <img src="${pokeman.image}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${pokeman.name}</h5>
+                <p class="card-text">
+                <strong>Type:</strong> ${pokeman.type}
+                </p>
+                <button type="button" class="btn btn-danger" id="modalBtn" data-bs-toggle="modal" onclick="getId(${pokeman.id})" data-bs-target="#exampleModal">
+                See Details
+                </button>
+            </div>
+          </div>
+            `
+        ).join('')
+        
+        pokedex.innerHTML = pokemonRender
+})
 
 
 /**
@@ -56,7 +65,7 @@ const displayPokemon = (pokemon) => {
                 <p class="card-text">
                 <strong>Type:</strong> ${pokeman.type}
                 </p>
-                <button type="button" class="btn btn-danger" id="modalBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <button type="button" class="btn btn-danger" id="modalBtn" data-bs-toggle="modal" onclick="getId(${pokeman.id})" data-bs-target="#exampleModal">
                 See Details
                 </button>
             </div>
@@ -67,27 +76,38 @@ const displayPokemon = (pokemon) => {
         pokedex.innerHTML = pokemonRender
 }
 
-const modalPopup = (pokemon) => {
-    const modalRender = pokemon.map((poke) => 
-        `
-        <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                    
-                    <h5 class="modal-title" id="${poke.name}"></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body" id="modalBody">
-            </div>
-            <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
-        </div>
+const getId = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url);
+    const pokeDetails = await res.json();
+    console.log(pokeDetails);
+    displayModal(pokeDetails)
+    /* const pokeDetails = data */
+}
+
+const displayModal = (pokeDetails) => {
+    const type = pokeDetails.types.map( type => type.type.name ).join(', ')
+    const abilities = pokeDetails.abilities.map( ability => ability.ability.name).join(', ')
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokeDetails.id}.png`
+    
+    modal.innerHTML = `
+    <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-2" id="exampleModalLabel">${pokeDetails.name}</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-        `
-    ).join('')
-    console.log(pokemon.name);
-    modal.innerHTML = modalRender
+      <div class="modal-body">
+      <img src="${image}" class="card-img-top">
+      <h1 class="modal-title fs-2" id="exampleModalLabel">${abilities}</h1>
+      <h3 class="modal-title fs-5" id="exampleModalLabel"<strong>Type: </strong>${type}</h3>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+    `
 }
 
 fetchPokemon()
